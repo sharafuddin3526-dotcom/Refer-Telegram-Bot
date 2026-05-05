d4d19f6d0af00453f4aef0da0be1523e9f199e28
sharafuddin3526-dotcom <sharafuddin3526@gmail.com>
2026-05-05 13:48:57 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
   BOT_TOKEN: "8663292414:AAE-8w0OwurrPENy3ntuZtfx9BLy37hu-6s",
-  CHANNELS: [
-    "@Global_Method_Channel"
-  ]
+  ADMIN_ID: 8136997138, // 👉 নিজের Telegram ID দাও
+  CHANNELS: ["@Global_Method_Channel"] // 👉 channel public username দিতে হবে
 };
