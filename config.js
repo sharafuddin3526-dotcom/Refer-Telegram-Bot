410a284bdf4aad27f82fc96e570d3784a556fd1f
sharafuddin3526-dotcom <sharafuddin3526@gmail.com>
2026-05-05 13:43:15 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  BOT_TOKEN: "8663292414:AAE-8w0OwurrPENy3ntuZtfx9BLy37hu-6s"
+  BOT_TOKEN: "8663292414:AAE-8w0OwurrPENy3ntuZtfx9BLy37hu-6s",
+  CHANNELS: [
+    "@Global_Method_Channel"
+  ]
 };
